e988676e4097fa24d4a1bd21ebc207dbd21d2d02
Abhijith <abhijithg@gmail.com>
2017-03-24 13:27:51 +0530

    representation of example ads and requests

--- rule.rb
+++ rule.rb
@@ -1,80 +1,99 @@
-rule = {
+# Example 1
+# ---------
+# A request comes with the following info:
+
+# Channel has category "cars";
+# User has preferences "cars","gadgets"
+
+# We have the following ads available:
+# Ad 1 -- category "cars"
+# Ad 2 -- Category "gadgets"
+
+# Returning Ad 1 is a better match since it doesn't contradict the channel's category
+
+# Example 2
+# ---------
+# Channel has category "cars"
+# User has preferences "gadgets"
+
+# Perhaps we don't return any ad since:
+
+# Ad 1 goes against the user's preferences
+# Ad 2 is not related to the channel
+
+
+# From here you can see that this starting point still allows you to
+# move on to more complex situations:
+
+# Categories can have hierarchies (gadgets can be phones, tablets,
+# watches). If there is a preference on a subcategory, that can imply a
+# preference on a parent category.
+
+# In my example 2, one could argue that it's less bad not to respect the
+# channel's category than the user's preference. So, if we can give the
+# user an ad they are interested on, even if that's not in the category
+# space of the channel, maybe that's a good alternative.
+
+class Expr
+  attr_accessor :channel, :advert, :operator
+end
+
+expr1 = {
+  :channel  => [:preferences],
+  :advert   => [:category]
+  :operator => :intersection,
+}
+
+expr2 = {
+  :channel  => [:category],
+  :advert   => [:category]
+  :operator => :==,
+}
+
+expr3 = {
+  :channel  => [:category],
+  :advert   => [:category]
+  :operator => :is_a?,
+}
+
+nested_rule = {
   :group => {
     :cond  => "AND" ,
     :rules => [
-      {
-        :attr     => "global-views",
-        :operator => ">=",
-        :value    => 1000
-      },
+      expr1,
       {
         :group => {
           :cond  => "OR",
-          :rules => [
-            {
-              :attr     => "preferences",
-              :operator => "in",
-              :value    => []
-            },
-            {
-              :attr     => "category",
-              :operator => "in",
-              :value    => []
-            },
-            {
-              :attr     => "user-views",
-              :operator => ">=",
-              :value    => "1000"
-            }]
+          :rules => [expr2, expr3]
         }
       }]
   }
 }
 
-# (AND
-#  (>= global-views 100)
-#  (OR
-#   (in preferences [])
-#   (in category [])
-#   (>= user-views 1000)))
-
-class Expr
-  attr_accessor :type, :request, :advert, :op
-end
-
-e1 = {
-  type:    :category,
-  request: [1],
-  advert:  [2],
-  op:      :debug
+flat_rule = {
+  :group => {
+    :cond  => "OR" ,
+    :rules => [expr1, expr2, expr3]
+  }
 }
 
-e2 = {
-  type:    :category,
-  request: [1],
-  advert:  [2],
-  op:      :m1
+# We have the following ads available:
+# Ad 1 -- category "cars"
+# Ad 2 -- Category "gadgets"
+a1 = {
+  category: ["cars"]
 }
 
-e3 = {
-  type:    :category,
-  request: [1],
-  advert:  [1],
-  op:      :m1
+a2 = {
+  category: ["gadgets"]
 }
 
-def debug(r, a)
-  p({r: r, a: a})
-end
+r1 = {
+  channel: "cars",
+  pref: ["cars", "gadgets"]
+}
 
-def m1(request, ads)
-  p request == ads
-end
-
-def foo(expr)
-  Object.send(expr[:op], expr[:request], expr[:advert])
-end
-
-foo(e1)
-foo(e2)
-foo(e3)
+r2 = {
+  channel: "cars",
+  pref: ["gadgets"]
+}
