935282598db5a0b1be8f25e23a57fedcae6eccc2
Abhijith <abhijithg@gmail.com>
2017-03-25 13:01:35 +0530

    moved example by luis into NOTE

--- rule.rb
+++ rule.rb
@@ -3,79 +3,12 @@ require_relative 'lib/expr'
 require_relative 'lib/channel'
 require_relative 'lib/advert'
 
-# Example 1
-# ---------
-# A request comes with the following info:
-
-# Channel has category "cars";
-# User has preferences "cars","gadgets"
-
-# We have the following ads available:
-# Ad 1 -- category "cars"
-# Ad 2 -- Category "gadgets"
-
-# Returning Ad 1 is a better match since it doesn't contradict the channel's category
-
-# Example 2
-# ---------
-# Channel has category "cars"
-# User has preferences "gadgets"
-
-# Perhaps we don't return any ad since:
-
-# Ad 1 goes against the user's preferences
-# Ad 2 is not related to the channel
-
-
-# From here you can see that this starting point still allows you to
-# move on to more complex situations:
-
-# Categories can have hierarchies (gadgets can be phones, tablets,
-# watches). If there is a preference on a subcategory, that can imply a
-# preference on a parent category.
-
-# In my example 2, one could argue that it's less bad not to respect the
-# channel's category than the user's preference. So, if we can give the
-# user an ad they are interested on, even if that's not in the category
-# space of the channel, maybe that's a good alternative.
-
 expr1 = Expr.new(channel: :pref, advert: :category, operator: :intersect?)
 expr2 = Expr.new(channel: :category, advert: :category, operator: :==)
 expr3 = Expr.new(channel: :category, advert: :category, operator: :subtype?)
 
 nested_rule = ExprGroup.new(:and, [expr1, ExprGroup.new(:or, [expr2, expr3])])
-
-# nested_rule = {
-#   :group => {
-#     :cond  => "AND" ,
-#     :rules => [
-#       expr1,
-#       {
-#         :group => {
-#           :cond  => "OR",
-#           :rules => [expr2, expr3]
-#         }
-#       }]
-#   }
-# }
-
-# flat_rule = {
-#   :group => {
-#     :cond  => "OR" ,
-#     :rules => [expr1, expr2, expr3]
-#   }
-# }
-
 flat_rule = ExprGroup.new(:or, [expr1, expr2, expr3])
-
-
-# Rule
-# channel-cat == advert-cat and ad-category in channel-pref
-# expr2 and expr1
-
-# We have the following ads available:
-# Ad 1 -- category "cars"
-# Ad 2 -- Category "gadgets"
 
 a1 = Advert.new(cat: ["cars"])
 a1.save
@@ -85,14 +18,7 @@ a2.save
 p Advert.all
 
 r1 = Channel.new(cat: ["cars"], preference: ["cars", "gadgets"])
-
-# Returning Ad 1 is a better match since it doesn't contradict the channel's category
-
 r2 = Channel.new(cat: ["cars"], preference: ["gadgets"])
-
-# Perhaps we don't return any ad since:
-# Ad 1 goes against the user's preferences
-# Ad 2 is not related to the channel
 
 class Array
 
