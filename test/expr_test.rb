a0975ebc87a858ada87ad0c9cdbfd471e49c87b9
Abhijith <abhijithg@gmail.com>
2017-03-28 15:35:38 +0530

    refactor

--- test/expr_test.rb
+++ test/expr_test.rb
@@ -10,19 +10,18 @@ class ExprTest < Test::Unit::TestCase
   def test_satisfies?
     assert_equal true,  @expr1.satisfies?(Request.new(channel: 1))
     assert_equal false, @expr1.satisfies?(Request.new(channel: 2))
+  end
 
+end
+
+class ExprGroupTest < ExprTest
+
+  def test_satisfies
     @expr3 = ExprGroup.new(:any?, [@expr1, @expr2])
     @expr4 = ExprGroup.new(:all?, [@expr1, @expr2])
 
     assert_equal true, @expr3.satisfies?(Request.new(channel: 1))
     assert_equal false, @expr3.satisfies?(Request.new(channel: 2))
-  end
-
-end
-
-class ExprGroupTest < Test::Unit::TestCase
-
-  def setup
   end
 
 end
