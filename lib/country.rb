5c787e41860d2aff232d627b4ab7a95f5733da90
Abhijith <abhijithg@gmail.com>
2017-03-28 10:26:08 +0530

    updated spec

---
+++ lib/country.rb
@@ -0,0 +1,41 @@
+class Country
+
+  attr_accessor :label
+
+  @@coll = []
+  @@counter = 0
+
+  def initialize(label)
+    @label = label
+  end
+
+  def self.load(file)
+    coll = JSON.parse(File.read(file), symbolize_names: true)
+    coll.map {|h| self.new(h).save }
+  end
+
+  def to_s
+  end
+
+  def self.count
+    @@counter
+  end
+
+  def save
+    @@counter = @@counter + 1
+    @@coll << self
+  end
+
+  def self.all
+    @@coll
+  end
+
+  def self.find(id)
+    @@coll[id]
+  end
+
+  def self.destroy_all
+    @@coll = []
+  end
+
+end
