232b44f69aacfac06a1c6985d36957062e4683a0
Abhijith <abhijithg@gmail.com>
2017-03-31 09:46:25 +0530

    limit refactor

--- lib/advert.rb
+++ lib/advert.rb
@@ -74,21 +74,19 @@ class Advert
   end
 
   def views_exhausted?(request)
-    # puts "--"
-    # p CountryLimit.all
-    # p [request, self]
-    # puts "--"
     a = CountryLimit.find(self.id, request.country).exhausted?
     b = ChannelLimit.find(self.id, request.channel).exhausted?
     a or b
   end
 
-  # def inc_view(request)
-  #   views = view.succ
-  #   [:country, :channel].each do |type|
-  #     Limit.find(ltype: type, ltype_id: request.send(type).id, advert_id: self.id)
-  #   end
-  # end
+  def views_exhausted?(request)
+    [:country, :channel].map do |type|
+      konstant = "#{type.capitalize}Limit"
+      if Object.const_defined?(konstant)
+        Object.const_get(konstant).send(:find, self.id, request.send(type)).exhausted?
+      end
+    end.any?
+  end
 
   def inc_view
     @views = @views + 1
@@ -100,15 +98,6 @@ class Advert
 
   def inc_channel_view(request)
     ChannelLimit.find(self.id, request.channel).inc_view
-  end
-
-  # make this polymorphic
-  def country_limits(country)
-    CountryLimit.all.select {|x| x.advert_id == self.id and x.country_id == country.id }
-  end
-
-  def channel_limits(channel)
-    ChannelLimit.all.select {|x| x.advert_id == self.id and x.channel_id == channel.id }
   end
 
   def set_country_limit(country, limit)
