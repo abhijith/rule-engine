5e5d7400442f346a1e1a1847b91ecad7afdda1cd
Abhijith <abhijithg@gmail.com>
2017-03-31 14:09:15 +0530

    test: live?

--- test/advert_test.rb
+++ test/advert_test.rb
@@ -63,6 +63,12 @@ class AdvertTest < Test::Unit::TestCase
     assert_equal true, @a1.live?
   end
 
+  def test_live
+    @a1.start_date = DateTime.now - 1000
+    @a1.end_date   = DateTime.now + 1000
+    assert_equal [@a1], Advert.live
+  end
+
   def test_exhausted?
     assert_equal false, @a1.exhausted?
   end
