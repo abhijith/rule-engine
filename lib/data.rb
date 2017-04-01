0afdb9ab06d85335cdb290c6dc069f7e8268fe47
Abhijith <abhijithg@gmail.com>
2017-04-01 15:13:20 +0530

    data-setup

---
+++ lib/data.rb
@@ -0,0 +1,39 @@
+def init_data
+  # cars
+  # |    `["bmw", "audi", "fiat", "volvo"]
+  # `bikes
+  # |    `["ktm", "bmw", "yamaha", "suzuki"]
+  # `foods
+  # |    `["dosa", "idly", "meatballs", "croissant", "pizza"]
+  # `flights
+  #       `["lufthansa", "air-france", "emirates", "air-india"]
+
+  categories = ["cars", "bikes", "food"]
+  cars       = ["bmw", "audi", "fiat", "volvo"]
+  bikes      = ["ktm", "bmw", "yamaha", "suzuki"]
+  flights    = ["lufthansa", "air-france", "emirates", "air-india"]
+  foods      = ["dosa", "idly", "meatballs", "croissant", "pizza"]
+
+  categories = {
+    "cars"   => cars,
+    "bikes"  => bikes,
+    "foods"  => foods,
+    "travel" => []
+  }
+
+  # channels
+  channels  = ["reddit.com", "team-bhp.com", "medium.com",
+              "trip-advisor.com", "booking.com"]
+  countries = ["india", "germany", "sweden", "france", "italy"]
+
+  # ads
+  ads = ["bmw-m4", "audi-a4", "fiat-punto", "volvo-s40",
+         "master-chef-australia", "master-chef-us", "ktm-390",
+         "yamaha-r6", "motocross", "formula-1", "khaana-kazana",
+         "airbnb", "euro-cars", "sixt", "hertz"]
+
+end
+
+def flush
+  [Advert, Channel, Country, Category, Limit].each(&:destroy_all)
+end
--- rpm.rb
+++ rpm.rb
@@ -2,49 +2,7 @@ require 'sinatra'
 
 require_relative 'lib/utils'
 require_relative 'lib/rpm'
-
-def init_data
-  ad1     = Advert.new(label: "bmw-m3").save
-
-  cars    = Category.new("cars")
-  bmw     = Category.new("bmw")
-  audi    = Category.new("audi")
-  food    = Category.new("food")
-  south   = Category.new("south-indian")
-  north   = Category.new("north-indian")
-
-  india   = Country.new(label: "india").save
-  germany = Country.new(label: "germany").save
-
-  [cars, bmw, food, audi, south, north].map(&:save)
-
-  cars.add_child(bmw)
-  cars.add_child(audi)
-
-  food.add_child(south)
-  food.add_child(north)
-
-  car_ex  = Channel.new(label: "car-example.com").save
-  food_ex = Channel.new(label: "food-example.com").save
-
-  car_ex.categories = [cars, food]
-
-  expr1 = Expr.new(field: :channel,    type: :channel,  value: 0, operator: :==)
-  expr2 = Expr.new(field: :country,    type: :country,  value: [0, 1], operator: :member?)
-  expr3 = Expr.new(field: :categories, type: :category, value: [0, 1], operator: :intersect?)
-
-  expr  = ExprGroup.new(:any?, [expr1, expr2, expr3])
-
-  ad1.constraints = expr
-
-  l1 = Limit.new(germany, 2)
-  l2 = Limit.new(car_ex, 2)
-  ad1.limits = [l1, l2]
-end
-
-def flush
-  [Advert, Channel, Country, Category, Limit].each(&:destroy_all)
-end
+require_relative 'lib/data'
 
 get '/' do
   flush
