dd575ac1c03c5ba34bb44430f0d42ead5df21e27
Abhijith <abhijithg@gmail.com>
2017-04-03 17:57:38 +0530

    request test

--- lib/request.rb
+++ lib/request.rb
@@ -5,8 +5,8 @@ class Request
   attr_accessor :channel, :categories, :country
 
   def initialize(channel: nil, categories: [], country: nil)
-    @channel    = Channel.find_by_label(channel) if channel
-    @country    = Country.find_by_label(country) if country
+    @channel    = Channel.find_by_label(channel)
+    @country    = Country.find_by_label(country)
     @categories = categories.map {|c| Category.find_by_label(c) }.compact
   end
 
--- test/request_test.rb
+++ test/request_test.rb
@@ -1,4 +1,28 @@
 require_relative 'utils'
 
 class RequestTest < Test::Unit::TestCase
+
+  def setup
+    @cat1 = Category.new("cars").save
+    @ch1  = Channel.new(label: "car-example.com").save
+    @india = Country.new(label: "india").save
+  end
+
+  def teardown
+    [Channel, Country, Category, Advert].each(&:destroy_all)
+  end
+
+  def test_initialize
+    @r1 = Request.new(channel: @ch1.label, categories: [@cat1.label], country: "india")
+
+    assert_equal @ch1,  @r1.channel
+    assert_equal @india, @r1.country
+    assert_equal [@cat1], @r1.categories
+
+    @r2 = Request.new(channel: "unknown.com", categories: ["bike"], country: "foo")
+    assert_equal nil, @r2.channel
+    assert_equal nil, @r2.country
+    assert_equal [],  @r2.categories
+  end
+
 end
