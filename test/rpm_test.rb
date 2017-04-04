bdbd3b7e110820836c780abf74a6b72e6ac0845e
Abhijith <abhijithg@gmail.com>
2017-04-04 17:24:54 +0530

    added main fn assertions

--- test/rpm_test.rb
+++ test/rpm_test.rb
@@ -88,27 +88,35 @@ class RpmTest < Test::Unit::TestCase
 
     ad = Advert.find_by_label("volvo-s40")
     r = Request.new(channel: "team-bhp.com", preferences: ["cars"], country: "sweden")
+    assert_equal ad, main({channel: "team-bhp.com", preferences: ["cars"], country: "sweden"})
     assert_equal true, ad.constraints.satisfies?(r)
 
     ad = Advert.find_by_label("bmw-i8")
     r = Request.new(channel: "team-bhp.com", preferences: ["bmw"], country: "germany")
+    assert_equal ad, main({channel: "team-bhp.com", preferences: ["bmw"], country: "germany"})
     assert_equal true, ad.constraints.satisfies?(r)
 
     ad = Advert.find_by_label("master-chef")
     r = Request.new(channel: "trip-advisor.com", preferences: ["food"], country: "india")
+    assert_equal ad, main({channel: "trip-advisor.com", preferences: ["food"], country: "india"})
     assert_equal true, ad.constraints.satisfies?(r)
     r = Request.new(channel: "trip-advisor.com", preferences: ["food"], country: "germany")
+    assert_equal ad, main({channel: "trip-advisor.com", preferences: ["food"], country: "germany"})
     assert_equal true, ad.constraints.satisfies?(r)
     r = Request.new(channel: "trip-advisor.com", preferences: ["food"], country: "sweden")
+    assert_equal ad, main({channel: "trip-advisor.com", preferences: ["food"], country: "sweden"})
     assert_equal true, ad.constraints.satisfies?(r)
 
 
     ad = Advert.find_by_label("air-berlin")
     r = Request.new(channel: "trip-advisor.com", preferences: ["cars"], country: "sweden")
+    assert_equal ad, main({channel: "trip-advisor.com", preferences: ["cars"], country: "sweden"})
     assert_equal true, ad.constraints.satisfies?(r)
     r = Request.new(channel: "trip-advisor.com", preferences: ["cars"], country: "germany")
+    assert_equal ad, main({channel: "trip-advisor.com", preferences: ["cars"], country: "germany"})
     assert_equal true, ad.constraints.satisfies?(r)
     r = Request.new(channel: "trip-advisor.com", preferences: ["cars"], country: "india")
+    assert_equal nil, main({channel: "trip-advisor.com", preferences: ["cars"], country: "india"})
     assert_equal false, ad.constraints.satisfies?(r)
   end
 
